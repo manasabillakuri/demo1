0eedb2b59a0e261c42d7ad84ab31ad74b582191b
manasabillakuri <131509936+manasabillakuri@users.noreply.github.com>
2023-04-22 23:46:43 +0530

    Add files via upload

---
+++ 7aprilGallary3.js
@@ -0,0 +1,13 @@
+function gallary()
+{
+    var photos=document.images;
+    for(i=0;i<=9;i++)
+    {
+      photos[i],src=`${i}.jpg`
+      console.log(photos[i])
+    }
+}
+function gal(loc)
+{
+    document.getElementById("bigImage").style.backgroundImage=`url(${loc})`
+}
